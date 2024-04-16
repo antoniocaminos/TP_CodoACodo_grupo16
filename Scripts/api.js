d8ad281874b7f91f6d32e938d4d2c0981ffca3a2
Antonio Caminos <75227866+antoniocaminos@users.noreply.github.com>
2024-04-16 13:28:03 -0300

    render de imagenes

--- Scripts/api.js
+++ Scripts/api.js
@@ -1,31 +1,33 @@
-/* const api_key = 'df5e2f1b886764d65236120a86ec7dd5'; 
-const api_url = 'https://api.themoviedb.org/3/';
- */
 const api_key = 'df5e2f1b886764d65236120a86ec7dd5';
+const baseImageUrl = 'http://image.tmdb.org/t/p/original';
 const url = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`;
- 
+
 const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
     Authorization: 'Bearer df5e2f1b886764d65236120a86ec7dd5'
   }
-  
 };
-  fetch(url)
+
+fetch(url)
   .then(response => response.json())
   .then(data => {
-    // Verificar si hay películas en los resultados
-    if (data.results && data.results.length > 0) {
-      // Iterar sobre las películas y mostrar la información de cada una
-      data.results.forEach(movie => {
-        console.log('Título:', movie.title);
-        console.log('ID:', movie.id);
-        // Agrega más información de la película si es necesario
-        console.log(movie.title);
-      });
-    } else {
-      console.log('No se encontraron películas.');
-    }
+    const moviesContainer = document.getElementById('movies-container');
+    data.results.forEach(movie => {
+      const movieCard = document.createElement('div');
+      movieCard.classList.add('col-md-4', 'mb-4');
+      movieCard.innerHTML = `
+        <div class="card">
+          <img src="${baseImageUrl}${movie.poster_path}" class="card-img-top" alt="${movie.title}">
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">${movie.overview}</p>
+            <p class="card-text"><small class="text-muted">Release Date: ${movie.release_date}</small></p>
+          </div>
+        </div>
+      `;
+      moviesContainer.appendChild(movieCard);
+    });
   })
-  .catch(err => console.error('Hubo un problema al obtener los datos:', err));
+  .catch(error => console.error('Error fetching data:', error));

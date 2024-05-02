ecbc81891253963a8065f77ee6a73958a37a54f8
Nahir Caridi <caridinahir@gmail.com>
2024-05-01 21:55:07 -0300

    corrijo typos en script.js y muevo import al inicio de Bootstrap Icons in style.css

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,5 +1,5 @@
 function fecha() {
         let fecha = new Date()
         let year = fecha.getFullYear()
-        document.getElementById("copyright").innerText = `Todos los derechos reervados @:`+year
+        document.getElementById("copyright").innerText = `Todos los derechos reservados @:`+year
 }fecha()
